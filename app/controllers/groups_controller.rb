018d5c4ceaaee9bd7443a47b136b3645b49824ac
chenxiaoqing <pincha120@gmail.com>
2017-07-02 17:32:38 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy, :join, :quit]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy,]
   def index
     @groups = Group.all
   end
@@ -22,6 +22,7 @@ class GroupsController < ApplicationController
     @group.user = current_user
 
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
